c3d7b3c3b6854d760bdd7316aa8db7645752883a
edwin.fullard <edwin.fullard@entelect.co.za>
2018-05-23 12:09:06 +0200

    Add reference bot compiled files

--- starter-pack/reference-bot/java/src/main/java/za/co/entelect/challenge/entities/GameDetails.java
+++ starter-pack/reference-bot/java/src/main/java/za/co/entelect/challenge/entities/GameDetails.java
@@ -5,12 +5,9 @@ import za.co.entelect.challenge.enums.BuildingType;
 import java.util.HashMap;
 
 public class GameDetails {
-
     public int round;
     public int mapWidth;
     public int mapHeight;
-    public int roundIncomeEnergy;
-    public HashMap<BuildingType, BuildingStats> buildingsStats = new HashMap<>();
-
+    public HashMap<BuildingType, Integer> buildingPrices;
 }
 
